3e0288d1b5d4623d98feba93415282e196951c61
XY8441ZE <ovidio.zealavado@colaborador.viajeseci.es>
2022-12-08 16:27:26 +0100

    Se crea indice repeticiones

--- 1.RecuperacionVectorial/VectorialIndex.py
+++ 1.RecuperacionVectorial/VectorialIndex.py
@@ -1,14 +1,8 @@
-#SGDI Practica 4 Ovidio Zea
-"""Declaramos que esta solución es fruto exclusivamente de nuestro
-trabajo personal. No hemos sido ayudados por ninguna otra persona
-ni hemos obtenido la solución de fuentes externas, y tampoco hemos
-compartido nuestra solución con otras personas. Declaramos además
-que no hemos realizado de manera deshonesta ninguna otra actividad
-que pueda mejorar nuestros resultados ni perjudicar los resultados
-de los demás.
-"""
+# Insertar aqui la cabecera
+
 import string
 import os
+import time
 from os.path import join, getsize
 # Dada una linea de texto, devuelve una lista de palabras no vacias 
 # convirtiendo a minusculas y eliminando signos de puntuacion por los extremos
@@ -19,53 +13,43 @@ def extrae_palabras(linea):
   return filter(lambda x: len(x) > 0, 
     map(lambda x: x.lower().strip(string.punctuation), linea.split()))
 
-class VectorialIndex2(object):
+
+class VectorialIndex(object):
+    indiceRepeticiones = {}
+    archivos = {}
+
     def __init__(self, directorio):
-        print(directorio)
-        numDocs = 0
-        archivos = {}
-        indiceRepeticiones = {}
+        start_time = time.time()
+        numDoc = 0 
+        count  = 0       
         for (root, dirs, files) in os.walk(directorio):
             for file in files:
                 filePath = root+"/"+file
-                archivos[numDocs] = filePath
+                self.archivos[numDoc] = filePath
                 content = open(filePath, "r", encoding="ISO-8859-1", errors="replace")
                 for line in content:
-                    for palabra in extrae_palabras(line):
-                        indiceRepeticiones[palabra]
-                        print(palabra)
-                numDocs += 1
-        print("Total files: ", numDocs)
-        print(archivos)
-                #print(open(filePath, "r", encoding="ISO-8859-1").read())
-                
-        pass
-
-class VectorialIndex(object):
-    def __init__(self, directorio):
-        print(directorio)
-        numDocs = 0
-        archivos = {}
-        indiceRepeticiones = {"ejemplo": [(0,1), (1,2)]}
-        if "ejemplo" in indiceRepeticiones :
-            print("valor ejemplo: ",indiceRepeticiones.get("ejemplo")[0])
-            indiceRepeticiones["ejemplo"].insert(0,(0,3))
-            indiceRepeticiones["ejemplo"].insert(3,(5,3))
-            indiceRepeticiones["ejemplo"].insert(3,(5,4))
-            indiceRepeticiones["ejemplo"].insert(2,(2,100))
-            indiceRepeticiones["ejemplo"][0] = (0,99) #posición cero de la lista 
-            print("valor Tupla posicion 0: ",indiceRepeticiones["ejemplo"][0][1])
-            print("valor Tupla posicion 5: ",indiceRepeticiones["ejemplo"][3][1])
-        else :
-            indiceRepeticiones["ejemplo2"] = [(0,3)]
-        print("Total files: ", numDocs)
-        print(archivos)
-        print(indiceRepeticiones)
-                #print(open(filePath, "r", encoding="ISO-8859-1").read())
+                    for word in extrae_palabras(line):
+                        if word in self.indiceRepeticiones :  #si ya existe la palabra en el indice                            
+                            tuple = dict(self.indiceRepeticiones[word]).get(numDoc)
+                            if tuple :# si ya existe el documento en la palabra
+                                count += 1 
+                                index = self.indiceRepeticiones[word].index((numDoc, tuple))
+                                tuple += 1                        
+                                self.indiceRepeticiones[word][index] = (numDoc,tuple)
+                            else :
+                                self.indiceRepeticiones[word].insert(numDoc,(numDoc,1))                        
+                        else :
+                            self.indiceRepeticiones[word] = [(numDoc,1)]
+                numDoc += 1
+        print("Total files: ", numDoc)
+        #print(self.archivos)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        #print(open(filePath, "r", encoding="ISO-8859-1").read())
                 
         pass
 
     def consulta_vectorial(self, consulta, n=3):
+        print(self.indiceRepeticiones)
         pass
 
     def consulta_conjuncion(self, consulta):
@@ -74,5 +58,7 @@ class VectorialIndex(object):
 if __name__ == "__main__" :
     print("Ovidio")
     vec = VectorialIndex("C:/SGDI/SGDI4/20news-18828")
+    #vec = VectorialIndex("C:/SGDI/20news-18828")
+    vec.consulta_vectorial("mathew", n=5)
     #vec = VectorialIndex("/home/usuario_vms/Icaro/SGDI4/20news-18828")
     #vec = VectorialIndex("/home/usuario_vms/Icaro/SGDI4/mini_collection")
